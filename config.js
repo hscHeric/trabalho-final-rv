b2fec304bdaf8bca39c6f3d125fbed64be62a709
Heric da Silva Cruz <hericsilvaho@gmail.com>
2026-06-12 14:42:53 -0300

    ajusta camera em tela cheia e localizacao

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ window.AR_POETRY_CONFIG = {
   title: "Estatua - Cego Aderaldo",
 
   // Troque pelas coordenadas reais da estatua.
-  latitude: -5.180266385480051,
-  longitude: -39.28633191359024,
+  latitude: -5.180301878427376,
+  longitude: -39.286202234377214,
 
   cubeSize: 10,
   cubeAltitude: 2,
